95e84bbd9266ccce82a95336ab4293b081fa8709
Tim Wagner <t.wagner@techdivision.com>
2019-12-17 16:39:44 +0100

    Fixed invalid command

--- src/Command/ImportCreateOkFileCommand.php
+++ src/Command/ImportCreateOkFileCommand.php
@@ -24,6 +24,7 @@ use TechDivision\Import\Utils\CommandNames;
 use TechDivision\Import\ConfigurationInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use TechDivision\Import\Cli\Utils\DependencyInjectionKeys;
 
 /**
  * The command implementation that creates a OK file from a directory with CSV files.
@@ -54,6 +55,31 @@ class ImportCreateOkFileCommand extends AbstractSimpleImportCommand
         parent::configure();
     }
 
+    /**
+     * Executes the current command.
+     *
+     * This method is not abstract because you can use this class
+     * as a concrete class. In this case, instead of defining the
+     * execute() method, you set the code to execute by passing
+     * a Closure to the setCode() method.
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface   $input  An InputInterface instance
+     * @param \Symfony\Component\Console\Output\OutputInterface $output An OutputInterface instance
+     *
+     * @return null|int null or 0 if everything went fine, or an error code
+     * @throws \LogicException When this abstract method is not implemented
+     * @see \Symfony\Component\Console\Command\Command::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        // initialize the configuration instance
+        $configuration = $this->getContainer()->get(DependencyInjectionKeys::CONFIGURATION_SIMPLE);
+
+        // finally execute the simple command
+        return $this->executeSimpleCommand($configuration, $input, $output);
+    }
+
 
     /**
      * Finally executes the simple command.
@@ -73,46 +99,54 @@ class ImportCreateOkFileCommand extends AbstractSimpleImportCommand
         // load the source directory, ALWAYS remove the directory separator, if appended
         $sourceDir = rtrim($configuration->getSourceDir(), DIRECTORY_SEPARATOR);
 
-        /** @var TechDivision\Import\Configuration\PluginConfigurationInterface $plugin */
-        foreach ($configuration->getPlugins() as $plugin) {
-            /** @var TechDivision\Import\Configuration\SubjectConfigurationInterface $subject */
-            foreach ($plugin->getSubjects() as $subject) {
-                // query whether or not, an OK file is needed
-                if ($subject->isOkFileNeeded()) {
-                    // load the prefix
-                    $prefix = $subject->getPrefix();
+        // load the array with the unique prefixes
+        $prefixes = $configuration->getPrefixes();
 
-                    // load the CSVfiles from the source directory
-                    $csvFiles = glob(sprintf('%s/%s*.csv', $sourceDir, $prefix));
+        // sort the prefixes
+        usort($prefixes, function ($a, $b) {
+            return strcmp($a, $b);
+        });
 
-                    // query whether or not any CSV files are available
-                    if (sizeof($csvFiles) > 0) {
-                        // prepare the OK file's content
-                        $okfileContent = '';
-                        foreach ($csvFiles as $filename) {
-                            $okfileContent .= basename($filename) . PHP_EOL;
-                        }
+        // initialize the counter for the CSV files
+        $csvFilesFound = 0;
 
-                        // prepare the OK file's name
-                        $okFilename = sprintf('%s/%s.ok', $sourceDir, $prefix);
+        // iterate over the prefixes and create the .ok files
+        foreach ($prefixes as $prefix) {
+            // load the CSVfiles from the source directory
+            $csvFiles = glob(sprintf('%s/%s_*.csv', $sourceDir, $prefix));
+            // raise the counter for the CSV files we've found
+            $csvFilesFound += sizeof($csvFiles);
+            // query whether or not any CSV files are available
+            if (sizeof($csvFiles) > 0) {
+                // prepare the OK file's content
+                $okfileContent = '';
+                foreach ($csvFiles as $filename) {
+                    $okfileContent .= basename($filename) . PHP_EOL;
+                }
 
-                        // write the OK file
-                        if (file_put_contents($okFilename, $okfileContent)) {
-                            // write a message to the console
-                            $output->writeln(sprintf('<info>Successfully written OK file %s</info>', $okFilename));
-                        } else {
-                            // write a message to the console
-                            $output->writeln(sprintf('<error>Can\'t write OK file %s</error>', $okFilename));
-                        }
-                    } else {
-                        // write a message to the console
-                        $output->writeln(sprintf('<error>Can\'t find any CSV files in source directory %s</error>', $sourceDir));
-                    }
+                // prepare the OK file's name
+                $okFilename = sprintf('%s/%s.ok', $sourceDir, $prefix);
 
-                    // stop the process
-                    return;
+                // write the OK file
+                if (file_put_contents($okFilename, $okfileContent)) {
+                    // write a message to the console
+                    $output->writeln(sprintf('<info>Successfully written OK file %s</info>', $okFilename));
+                } else {
+                    // write a message to the console
+                    $output->writeln(sprintf('<error>Can\'t write OK file %s</error>', $okFilename));
                 }
             }
         }
+
+        // query whether or not we've found any CSV files
+        if ($csvFilesFound === 0) {
+            // write a message to the console, if we can't find any CSV files
+            $output->writeln(sprintf('<error>Can\'t find any CSV files in source directory %s</error>', $sourceDir));
+            // return 1 to signal an error
+            return 1;
+        }
+
+        // return 0 to signal success
+        return 0;
     }
 }
