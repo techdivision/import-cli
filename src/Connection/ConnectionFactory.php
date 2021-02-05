e07f8fbdea6172c4651247c5f12b5159e6fc3853
Tim Wagner <t.wagner@techdivision.com>
2021-02-05 11:27:02 +0100

    Add @deprecated annotations for upcoming 12.x branch

--- src/Connection/ConnectionFactory.php
+++ src/Connection/ConnectionFactory.php
@@ -26,11 +26,13 @@ use TechDivision\Import\Connection\PDOConnectionWrapper;
 /**
  * The connection factory implementation.
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2016 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-cli-simple
- * @link      http://www.techdivision.com
+ * @author     Tim Wagner <t.wagner@techdivision.com>
+ * @copyright  2021 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/import-cli-simple
+ * @link       http://www.techdivision.com
+ * @deprecated Since 11.1.0
+ * @see        TechDivision\Import\Dbal\Connection\ConnectionFactory
  */
 class ConnectionFactory
 {
